3d224163cb15da5df42986d20a4ec85c8bd08d96
Guilherme Anacleto Ferreira <guilhermeanacleto@hotmail.com>
2021-07-12 11:32:22 -0300

    Desgin Done

--- app/routes.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask 
-
-def routes(app):
-    @app.route('/')
-    def index():
-        return 'hello world'
--- app/run.py
+++ app/run.py
@@ -1,14 +1,14 @@
 import os
-from flask import Flask
-from app.routes import routes
-
+from flask import Flask, render_template, url_for
 
 def create_app():
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
 
-    routes(app)
-
+    @app.route('/')
+    def index():
+        return render_template('index.html', password = "guigasgo")
+    
     return app
 
 app = create_app()
