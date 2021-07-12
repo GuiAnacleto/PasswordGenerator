3e5245c3355f9558a4d92cc6f73904ef99b8def2
Guilherme Anacleto Ferreira <guilhermeanacleto@hotmail.com>
2021-07-12 11:54:36 -0300

    Final Commit

--- app/run.py
+++ app/run.py
@@ -1,5 +1,8 @@
 import os
 from flask import Flask, render_template, url_for
+import random
+import string
+
 
 def create_app():
     # create and configure the app
@@ -7,7 +10,30 @@ def create_app():
 
     @app.route('/')
     def index():
-        return render_template('index.html', password = "guigasgo")
+        return render_template('index.html', password = gen_password())
+
+    def gen_password(): 
+ 	    #input the length of password
+        length = 8
+
+        #define data
+        lower = string.ascii_lowercase
+        upper = string.ascii_uppercase
+        num = string.digits
+        symbols = string.punctuation
+        #string.ascii_letters
+
+        #combine the data
+        all = lower + upper + num + symbols
+
+        #use random 
+        temp = random.sample(all,length)
+
+        #create the password 
+        password = "".join(temp)
+
+        #print the password
+        return password
     
     return app
 
